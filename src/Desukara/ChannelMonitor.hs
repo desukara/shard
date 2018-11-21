08aca9b4a4911f959fd62a5008acb5f2af4c6394
Andy Chun @noneucat <andy@lolc.at>
2018-11-21 11:26:58 -0800

    Added a couple of newlines in the onboarding message

--- src/Desukara/ChannelMonitor.hs
+++ src/Desukara/ChannelMonitor.hs
@@ -49,28 +49,30 @@ monitor chan dis ctx =
 
                         restCall dis (CreateMessage defaultchannel "" 
                             $ Just Embed {
-                                embedTitle = "Hi there!",
+                                embedTitle = "Dearest server owner:",
                                 embedType = "rich",
                                 embedDesc = intercalate "  \n" 
-                                [   "**Thanks for the invite!** Before we get started, here are a couple things you should know:"
-
+                                [   "**Thanks for the invite!**"
+                                
+                                ,   "Before we get started, here are a couple things you should know:"
+                                ,   ""
                                 ,   "- I won't aggregate messages from channels unless you *explicitly enable them*. Use `ds@enableChannel` "
                                 ++  "in the appropriate channel to *enable aggregation of all messages available* in the channel "
                                 ++  "*(in accordance with Section 2.5d of the Discord Developer ToS)*. This is required to use any data-related "
                                 ++  "commands in a channel."
-
+                                ,   ""
                                 ,   "- I can take ~10 to 20 minutes to completely aggregate messages from a channel after enabling. "
                                 ++  "It's wisest to wait in order to get accurate reports."
-
+                                ,   ""
                                 ,   "- I'll aggregate *all messages available* from enabled channels for *as log as the the channel is enabled* "
                                 ++  "in order to generate accurate reports (with exceptions as defined by Section 2.4 & 2.5 of the Discord Developer ToS)*. "
                                 ++  "Channels disabled using `ds@disableChannel` will have their data pruned as soon as possible."
-
+                                ,   ""
                                 ,   "- I'll prune all messages from this guild as soon as possible if I leave the guild "
                                 ++  "*(in accordance with Section 2.4 of the Discord Developer ToS)*."
-
                                 ,   "" 
-                                ,   "Thanks, and have fun!"
+                                ,   "That's all! Thanks, and have fun!"
+                                ,   ""
                                 ,   "*~ Sleepy, 2nd Class Angel*"
                                 ],
                                 embedUrl = "",
