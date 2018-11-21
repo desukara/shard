7aa85008a4c43e260e116c4d9763b459a6581085
Andy Chun @noneucat <andy@lolc.at>
2018-11-20 21:48:12 -0800

    Implem. admin command logic & added commands to enable/disable channels.

--- src/Desukara/MessageManager.hs
+++ src/Desukara/MessageManager.hs
@@ -7,7 +7,8 @@ module Desukara.MessageManager (
 import Control.Monad (when)
 import Control.Concurrent (forkIO, threadDelay)
 import Control.Concurrent.Chan
-import Data.List (isInfixOf)
+import Data.Bits
+import Data.List (isInfixOf, intersect)
 import Data.Time.Clock
 import Data.Time.Format
 import qualified Data.Text as T
@@ -18,6 +19,8 @@ import Discord
 import qualified Discord as DS
 
 import DbLib
+import DbLib.GuildDataDb.Channels
+import qualified DbLib.GuildDataDb.Channels as DB
 import DbLib.JobManagerDb.Jobs
 
 type TotalJobRunners = Int
@@ -41,7 +44,8 @@ messageManager botid totalrunners chan dis ctx =
                         -- todo configurable
                         mentioned = [] /= filter (\u -> show (userId u) == botid) mentions
 
-                    when mentioned (handleMessage m)
+                    -- when mentioned (handleMessage m)
+                    handleMessage m
             _ -> return ()
         
         messageManager botid totalrunners chan dis ctx -- todo: repeatedly?
@@ -50,13 +54,21 @@ messageManager botid totalrunners chan dis ctx =
         handleMessage msg = 
             do
                 let text = T.unpack $ messageText msg
-                    user = userName $ messageAuthor msg
+                    user = messageAuthor msg
+                    username = userName user
+                    userid = userId user
+                    maybeGuild = messageGuild msg
                     ds_channel = messageChannel msg
+                    channelId = show ds_channel
 
-                    sendMessage t =
+                    sendMessage t = do
                         restCall dis (CreateMessage ds_channel t Nothing) 
+                        return ()
 
-                if | "eval" `isInfixOf` text -> 
+                -- todo: really inefficient...
+                isChannelEnabled <- fmap ((channelId `elem`) . (map DB.channelId)) $ getActiveChannels ctx
+
+                if | "ds!eval" `isInfixOf` text && isChannelEnabled -> 
                     do
                         let mkRegex' s = mkRegexWithOpts s False False 
                             rRegex = mkRegex' "```r(.*)```"
@@ -65,13 +77,11 @@ messageManager botid totalrunners chan dis ctx =
                             Just matches -> do
                                 let code = head matches 
 
-                                sendMessage "Sure thing!"
-
                                 winner <- randomRIO (0, totalrunners - 1) -- todo allocate smarter?
                                 
                                 currentTime <- getCurrentTime
                                 createJob ctx defaultJob {
-                                    jobTitle = user ++ "'s Query",
+                                    jobTitle = username ++ "'s Query",
                                         -- ++ formatTime defaultTimeLocale "%D, %l:%M%P UTC" currentTime,
                                     jobOwner = winner,
                                     jobChannel = show (messageChannel msg),
@@ -85,6 +95,47 @@ messageManager botid totalrunners chan dis ctx =
                                 return ()
 
                         return ()
+                   | "ds@" `isInfixOf` text -> do
+                        -- check perms
+                        case maybeGuild of
+                            Just guildId -> do
+                                -- get guild owner
+                                guildOwner <- do 
+                                    maybeGuild <- restCall dis (GetGuild guildId)
+                                    return $ case maybeGuild of
+                                        Right guild -> show $ guildOwnerId guild
+                                        Left _ -> ""
+
+                                -- get admin roles
+                                adminRoles <- do
+                                   maybeRoles <- restCall dis (GetGuildRoles guildId)
+                                   return $ case maybeRoles of
+                                        Right roles -> map roleID $
+                                            filter (((>) 0) . ((.&.) 8) . rolePerms) roles
+                                        Left _ -> []
+                                g <- restCall dis (GetGuildMember guildId (userId user)) 
+                                
+                                case g of
+                                    Left _ -> return ()
+                                    Right guildMember -> do
+                                        let isAdmin = (show userid == guildOwner)
+                                                   || (not . null $ memberRoles guildMember `intersect` adminRoles)
+                                        if isAdmin
+                                            -- verified!
+                                            then if | "ds@enableChannel" `isInfixOf` text -> 
+                                                        do 
+                                                            setChannelEnabled ctx channelId True
+                                                            sendMessage . T.pack $ "Enabled indexing and queries in this channel!"
+                                                            return ()
+                                                    | "ds@disableChannel" `isInfixOf` text ->
+                                                        do
+                                                            setChannelEnabled ctx channelId False 
+                                                            sendMessage . T.pack $ "Disabled indexing and queries in this channel!"
+                                                            return ()
+                                                    | True -> return ()
+                                            else sendMessage "Sorry, you don't have the permissions to do that..."
+                                        return ()
+                            Nothing -> return ()
                    | True -> return ()
 
 
