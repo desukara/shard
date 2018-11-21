70eb715b6dbd9855c14368d4e38e7f72a3c1f3e0
Andy Chun @noneucat <andy@lolc.at>
2018-11-20 22:01:53 -0800

    Fixed bug where channelGuild would be set to 0 when reconnecting to Discord

--- src/Desukara/ChannelMonitor.hs
+++ src/Desukara/ChannelMonitor.hs
@@ -20,18 +20,19 @@ monitor chan ctx =
         case e of
             Left _      -> return ()
             Right event -> case event of
-                ChannelCreate dc -> updateDb dc
-                ChannelUpdate dc -> updateDb dc
-                GuildCreate g gi -> mapM_ updateDb (guildChannels gi)
+                ChannelCreate dc -> updateDb dc (DS.channelGuild dc)
+                ChannelUpdate dc -> updateDb dc (DS.channelGuild dc)
+                GuildCreate g gi -> mapM_ (\dc -> updateDb dc (guildId g)) (guildChannels gi)
                 _ -> return ()
 
         monitor chan ctx
     where
-        updateDb dc = case dc of
+        updateDb dc guild = case dc of
             ChannelText _ _ _ _ _ _ _ ->
                 DB.saveChannel ctx Channel {
                     DB.channelId = show $ DS.channelId dc,
-                    DB.channelGuild = show $ DS.channelGuild dc,
+                    -- DB.channelGuild = show $ DS.channelGuild dc,
+                    DB.channelGuild = show $ guild,
                     DB.channelName = DS.channelName dc,
                     DB.channelEnabled = Nothing,
                     DB.channelPriority = Nothing
